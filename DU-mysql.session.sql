229e8c55980344ff314a14d483015a4d3c3ea305
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2026-07-26 23:15:21 +0530

    a

--- DU-mysql.session.sql
+++ DU-mysql.session.sql
@@ -1,4 +1,14 @@
-SELECT e.*, d.*
-FROM emp AS e
-JOIN dept AS d
-	ON e.deptno = d.deptno;
+SELECT
+    e.empno,
+    e.ename,
+    e.job,
+    e.sal,
+    e.deptno
+FROM emp e
+WHERE e.deptno IN (
+    SELECT deptno
+    FROM emp
+    GROUP BY deptno
+    HAVING COUNT(*) > 5
+)
+ORDER BY e.deptno, e.empno;
